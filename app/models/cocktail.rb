2b611fe622fc77d4f2f9ea1bfcc4c479243497e3
Hugo Dornbierer <hdornbierer@gmail.com>
2020-02-13 11:24:42 +0100

    Created and validated models and their associations

---
+++ app/models/cocktail.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Comment
+class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses, dependent: :restrict_with_error
+
+  validates :name, presence: true, uniqueness: true
+end
---
+++ app/models/dose.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Comment
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates_uniqueness_of :cocktail, scope: [:ingredient]
+end
